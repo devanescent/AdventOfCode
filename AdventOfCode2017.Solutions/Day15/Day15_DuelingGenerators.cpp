150200cec01e1ecddbe7186c815fa9afa6b7f507
devanescent <devanescent@gmail.com>
2023-10-20 21:06:53 +0200

    Added 2017 day 15 (part 2)

--- AdventOfCode2017.Solutions/Day15/Day15_DuelingGenerators.cpp
+++ AdventOfCode2017.Solutions/Day15/Day15_DuelingGenerators.cpp
@@ -32,6 +32,33 @@ namespace AdventOfCode::Year2017::Day15
 
 	uint64_t DuelingGenerators::GetResultOnPart2(std::vector<std::string> input)
 	{
-		return uint64_t();
+		// Initial values for the generators:
+		uint64_t genA, genB;
+		std::string discard;
+
+		std::istringstream issA(input[0]);
+		issA >> discard >> discard >> discard >> discard >> genA;
+
+		std::istringstream issB(input[1]);
+		issB >> discard >> discard >> discard >> discard >> genB;
+
+		// Generate values and compare:
+		uint64_t matches = 0;
+		for (int i = 0; i < 5'000'000; ++i)
+		{
+			do
+			{
+				genA = (genA * 16807) % 2147483647;
+			} while (genA % 4 != 0);
+			
+			do
+			{
+				genB = (genB * 48271) % 2147483647;
+			} while (genB % 8 != 0);
+
+			matches += (genA & 0xFFFF) == (genB & 0xFFFF) ? 1 : 0;
+		}
+
+		return matches;
 	}
 }
--- AdventOfCode2017.Tests/Day15_Test.cpp
+++ AdventOfCode2017.Tests/Day15_Test.cpp
@@ -19,8 +19,8 @@ namespace AdventOfCode::Year2017::Tests
 				"Generator A starts with 65",
 				"Generator B starts with 8921"
 			};
-
 		};
+
 		TEST_METHOD(CheckExample1_Part1)
 		{
 			// Arrange:
@@ -31,6 +31,15 @@ namespace AdventOfCode::Year2017::Tests
 			Assert::AreEqual(588ull, result);
 		};
 
+		TEST_METHOD(CheckExample1_Part2)
+		{
+			// Arrange:
+			DuelingGenerators sut;
+			// Act:
+			uint64_t result = sut.GetResultOnPart2(inputData1);
+			// Assert:
+			Assert::AreEqual(309ull, result);
+		};
 	};
 	std::vector<std::string> Year2017_Day15::inputData1 = std::vector<std::string>();
 }
