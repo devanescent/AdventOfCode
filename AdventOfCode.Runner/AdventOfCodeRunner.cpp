42562a4d37e513cfb058c07fc3db1fc2f5086ed2
devanescent <devanescent@gmail.com>
2024-12-16 21:13:20 +0100

    Finally added error handling when working directory does not contain Input.txt file; added automatic caching of input files

--- AdventOfCode.Runner/AdventOfCodeRunner.cpp
+++ AdventOfCode.Runner/AdventOfCodeRunner.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <format>
+#include <filesystem>
 #include "SolutionFactory.h"
 #include "toml.hpp"
 
@@ -11,14 +13,13 @@ using namespace AdventOfCode;
 // ---------------------------------------------------------------------------
 // ReadLinesFromFile: Process input file to string lines
 // ---------------------------------------------------------------------------
-std::vector<std::string> ReadLinesFromFile()
+std::vector<std::string> ReadLinesFromFile(std::ifstream& input)
 {
 	std::vector<std::string> lines;
 	lines.reserve(1000);
 
-	std::ifstream inFile("Input.txt");
 	std::string line;
-	while (std::getline(inFile, line))
+	while (std::getline(input, line))
 		lines.push_back(line);
 
 	return lines;
@@ -29,18 +30,62 @@ std::vector<std::string> ReadLinesFromFile()
 // ---------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
+	if (!std::filesystem::exists("appconfig.toml"))
+	{
+		std::cout << "Error: No config file (appconfig.toml) found in current / working directory\n";
+		return -1;
+	}
+
 	// Read which year / day / part to run from config:
 	auto appConfig = toml::parse_file("appconfig.toml");
+
 	int year = appConfig["run"]["year"].value_or(2023);
 	int day = appConfig["run"]["day"].value_or(1);
 	int part = appConfig["run"]["part"].value_or(1);
+
+	bool useCache = appConfig["caching"]["use_cache"].value_or(false);
+	std::string cacheDirectory = appConfig["caching"]["cache_dir"].value_or("");
 
 	// Solution of the given year / day:
 	auto solution = SolutionFactory().GetYear(year)->GetDay(day);
 	if (solution)
 	{
-		// Read input and get results:
-		auto lines = ReadLinesFromFile();
+		// Default input name:
+		std::string inputFileName("Input.txt");
+
+		// Cache file:
+		std::string cacheFileName = std::format("Input_{}_{}.txt", year, day);
+		auto cacheFileLocation = std::filesystem::path(cacheDirectory) / cacheFileName;
+
+		if (useCache && !cacheDirectory.empty())
+		{
+			if (!std::filesystem::exists(cacheFileLocation))
+			{
+				std::cout << "No cached input available for " << year << "/" << day << "\n";
+				return -1;
+			}
+			else
+			{
+				// Replace default input with cached input file:
+				inputFileName = cacheFileLocation.string();
+			}
+		}
+		else
+		{
+			if (!std::filesystem::exists("Input.txt"))
+			{
+				std::cout << "Error: No input file (Input.txt) found in current / working directory\n";
+				return -1;
+			}
+
+			// Cache current input file for later use:
+			if (!cacheDirectory.empty() && !std::filesystem::exists(cacheFileLocation))
+				std::filesystem::copy("Input.txt", cacheFileLocation);
+		}
+
+		// Read input (or use cached input data):
+		std::ifstream inputFile(inputFileName);
+		auto lines = ReadLinesFromFile(inputFile);
 		auto res = part == 1 ? solution->GetResultOnPart1(lines) : solution->GetResultOnPart2(lines);
 
 		// Print results to screen:
